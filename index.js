99b510b330122630706ba8693028ad9bc52a86b4
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2022-10-25 13:35:19 +0600

    load all courses data

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const cors = require('cors')
+
 app.use(cors())
 
+const courses = require('./data/courses.json')
 
 app.get('/', (req, res) =>{
     res.send('I am creating a server for programming workshop website!!')
@@ -10,5 +13,10 @@ app.get('/', (req, res) =>{
 
 app.listen(port, () =>{
     console.log('Running form port: ', port)
+})
+
+// load all courses
+app.get('/courses', (req, res) =>{
+    res.send(courses)
 })
 
